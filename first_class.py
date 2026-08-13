d48b610599ca9f3ae107635274317c4098b07388
riri2006 <riddhiverma001@gmail.com>
2026-08-13 13:49:36 +0530

    python

---
+++ first_class.py
@@ -0,0 +1,15 @@
+# def greet():
+#     print("Hello!")
+
+# call = greet
+# call()
+
+
+# def student():
+#     print("This is student")
+
+# def call(func):
+#     func()
+
+# abc = call
+# abc(student)
