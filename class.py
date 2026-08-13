9ba7d0616d50fddbd5408d79778cdb20c8f1c591
riri2006 <riddhiverma001@gmail.com>
2026-08-13 22:47:02 +0530

    python

---
+++ class.py
@@ -0,0 +1,31 @@
+class Kapil:
+    def __init__(self, name):
+    
+        self.name = name
+        self.him("Sharda sasumummaa", "Praveen sasurpapa", "Khushi", "Poornima", "Gopal taujiii", "Manju mosii")
+
+    def me(self, name1):
+        self.name1 = name1
+        print("My Name: ", self.name1)
+
+    def him(self, mom, dad, sis1, sis2, tauji, taiji):
+        self.mom = mom
+        self.tauji = tauji
+        self.taiji = taiji
+        self.dad = dad
+        self.sis1 = sis1
+        self.sis2 = sis2
+        print("His Name: ", self.name)
+        print("Mother is law's Name: ", self.mom)
+        print("Father in law's Name: ", self.dad)
+        print("Sister in law's Name: ", self.sis1)
+        print("Sister in law's Name: ", self.sis2)
+        print("Tauuji in law's Name: ", self.tauji)
+        print("Taiji in law's Name: ", self.taiji)
+
+object = Kapil("Vedant")
+object.me("R Kapil")
+
+                
+        
+        
--- first_class.py
+++ first_class.py
@@ -1,15 +1,63 @@
-# def greet():
-#     print("Hello!")
+# # def greet():
+# #     print("Hello!")
 
-# call = greet
-# call()
+# # call = greet
+# # call()
 
 
-# def student():
-#     print("This is student")
+# # def student():
+# #     print("This is student")
 
-# def call(func):
-#     func()
+# # def call(func):
+# #     func()
 
-# abc = call
-# abc(student)
+# # abc = call
+# # abc(student)
+
+# # def mult(a,b):
+# #     return a*b
+
+# # def call(fun,x,y):
+# #     return fun(x,y)
+
+# # result = call(mult,10,2)
+# # print(result)
+
+
+# def mult(a,b):
+#     print("product: ", a*b)
+
+# def call(fun,x,y):
+#     return(fun(x,y))
+
+# result = call(mult,10,2)
+# result
+
+
+
+# class Calculator:
+
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+#         print("hello")    
+
+
+# def add(calc: Calculator):
+#     print(calc.a + calc.b)
+
+
+# c1 = Calculator(10, 20)
+
+# add(c1)
+
+
+
+def outer():
+    def inner():
+        print("Inner function")
+
+    return inner
+
+my_function = outer()
+my_function()
