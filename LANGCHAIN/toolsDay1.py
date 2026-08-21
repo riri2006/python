526cced72bcfc789dea74f6ba957591e197ca9b6
riri2006 <riddhiverma001@gmail.com>
2026-08-21 22:06:52 +0530

    toolss

---
+++ LANGCHAIN/toolsDay1.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import MemorySaver
+from langchain.tools import tool
+
+llm = ChatGroq(model = "openai/gpt-oss-20b")
+memory = MemorySaver()
+
+@tool
+def add(a: int, b: int):
+    """Adding numbers"""
+    return a+b
+
+agent = create_agent(
+    model = llm,
+    checkpointer= memory,
+    tools= [add]
+
+)
+config = { "configurable" : { "thread_id" : "1"}}
+
+while True:
+    query = input("Ask your question: ")
+    if query in ["terminate", "exit", "quit"]:
+        print("Terminating the conversation..thank u")
+        break
+    response = agent.invoke({
+        "messages":[{"role": "user", "content": query }]
+    },config = config)
+
+    print("AI: ",response['messages'][-1].content)
