e0aa30d6e98df036a95585c796ac59edeeef9c83
riri2006 <riddhiverma001@gmail.com>
2026-08-25 22:46:35 +0530

    numpy day2

---
+++ numpy/Day02.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#created array
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([2,5,6,1])
+#operations on array
+print(arr1) 
+print(arr1*5) #multiplying array with a number
+print(arr1+10) #adding array to any number
+print(arr1+arr2) #addition of 2 arrays
+print(arr1*arr2) #multiplying 2 arrays
