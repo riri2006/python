073013f1941dc006fdc96c2cb40734647acbda05
riri2006 <riddhiverma001@gmail.com>
2026-09-02 23:40:29 +0530

    StrictInt

--- LANGCHAIN/pydanticday1.py
+++ LANGCHAIN/pydanticday1.py
@@ -8,3 +8,16 @@ class Student(BaseModel):
 student = Student(name="Riddhi", age="20")
 print(student)
 
+##strictInt 
+class Fruit(BaseModel):
+    name: str
+    units: StrictInt
+fruit1 = Fruit(name="Mango", units=100)
+print(fruit1)
+try:
+    fruit2 = Fruit(name="Lychee", units="100")
+    print(fruit2)
+except :
+    print("Invalid data type")
+
+
