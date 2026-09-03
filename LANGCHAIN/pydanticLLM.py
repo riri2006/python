77327816fc16f7ca1ab804be22a032454b1285a3
riri2006 <riddhiverma001@gmail.com>
2026-09-03 21:15:58 +0530

    llm with pydantic

--- LANGCHAIN/pydanticLLM.py
+++ LANGCHAIN/pydanticLLM.py
@@ -11,5 +11,7 @@ llm= ChatGroq(model="openai/gpt-oss-20b")
 
 structured_llm = llm.with_structured_output(Person)
 
-result = structured_llm.invoke("Hi my name is Riddhi and i am 20 years old")
+# result = structured_llm.invoke("Hi my name is Riddhi and i am 20 years old")
+query= input("ASK: ")
+result = structured_llm.invoke(query)
 print(result)
