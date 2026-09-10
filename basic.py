b94943402d66d63d5d282dcaa352bd838be99ca4
riri2006 <riddhiverma001@gmail.com>
2026-09-10 22:16:39 +0530

    basic

---
+++ basic.py
@@ -0,0 +1,27 @@
+#strings lists dictionary and other basic 
+
+string1 = "Hello my name is Pizza.."
+string2="I am Burger.."
+print()
+print("STRING")
+print("Original String:", string1)
+print("string1[3]: ", string1[3])
+print("string1[1:5]:",string1[1:5] )
+print("string1[6:]:",string1[6:])
+print("string1*2:", string1*2)
+print("concatenation: ", string1+string2)
+
+list1 =["123riddhi","124vedant"]
+list2=["riddhi", "vedant"]
+print()
+print("LIST")
+print(list1)
+print(list2[0])
+print(list1[1])
+print(list1+list2)
+print(list2*2)
+
+dict1={"name":"Riddhi", "age":20}
+print("\nDICTIONARY")
+print(dict1.keys())
+print(dict1.values())
